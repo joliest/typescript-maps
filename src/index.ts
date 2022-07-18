45558a721364644c2942a8b5704ccc4be52542ef
Joliver Estampador <joliverestampador@gmail.com>
2022-07-19 07:42:26 +0800

    added google.maps library and some cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,2 @@
-import { User } from './User';
-import { Company } from './Company';
-
-const user = new User();
-const company = new Company();
-
-console.log(user);
-console.log(company);
+// import { User } from './User';
+// import { Company } from './Company';
