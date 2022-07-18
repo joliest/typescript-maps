17dfe89cf75870a333468fd212eda78f856c5030
Joliver Estampador <joliverestampador@gmail.com>
2022-07-19 07:54:06 +0800

    adding the map onto the page

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
 // import { User } from './User';
 // import { Company } from './Company';
+
+new google.maps.Map(document.getElementById('map') as HTMLElement, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
