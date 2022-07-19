a381f4314a53df1ca94c096631e08526ee93a52b
Joliver Estampador <joliverestampador@gmail.com>
2022-07-20 07:42:01 +0800

    CUstom map class

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 // import { User } from './User';
 // import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
-new google.maps.Map(document.getElementById('map') as HTMLElement, {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
+/**
+ * Generates a map
+ */
+new CustomMap('map');
